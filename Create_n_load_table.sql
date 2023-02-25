d44941956cdd98f86ba98e1ee4790a01526babb7
bagusakhlaq <51534265+bagusakhlaq@users.noreply.github.com>
2023-02-25 12:23:53 +0700

    Update Create_n_load_table.sql

--- Create_n_load_table.sql
+++ Create_n_load_table.sql
@@ -1,20 +1,20 @@
 # CREATE TABLE
 USE cyclistic;
 CREATE TABLE td_2110 (
-		ride_id TEXT,
-    rideable_type TEXT DEFAULT NULL,
-    started_at DATETIME,
-    ended_at DATETIME,
-    start_station_name TEXT DEFAULT NULL,
-    start_station_id TEXT DEFAULT NULL,
-    end_station_name TEXT DEFAULT NULL,
-    end_station_id TEXT DEFAULT NULL, 
-    start_lat DOUBLE,
-    start_lng DOUBLE,
-    end_lat DOUBLE,
-    end_lng DOUBLE,
-    member_casual TEXT DEFAULT NULL
-    );
+	ride_id TEXT,
+	rideable_type TEXT DEFAULT NULL,
+	started_at DATETIME,
+	ended_at DATETIME,
+	start_station_name TEXT DEFAULT NULL,
+	start_station_id TEXT DEFAULT NULL,
+	end_station_name TEXT DEFAULT NULL,
+	end_station_id TEXT DEFAULT NULL, 
+	start_lat DOUBLE,
+	start_lng DOUBLE,
+	end_lat DOUBLE,
+	end_lng DOUBLE,
+	member_casual TEXT DEFAULT NULL
+	);
 
 # ----------------------------------------------------------------------------------------------
 # LOAD THE DATA INTO THE TABLE
